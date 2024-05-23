d3f7aa983e3c74f468b04c11daa8c64909a6dded
kelyan danis <kelyan.danis@ynov.com>
2024-05-23 10:06:49 +0200

    ✨ i add post gestion, need to be tested ✨

---
+++ CLIENT/post.go
@@ -0,0 +1,112 @@
+package client
+
+import (
+	"database/sql"
+	"strings"
+
+	//"html/template"
+	"log"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type post struct {
+	username 	string
+	message    	string
+	image	    string
+	date		string
+	like 		int
+	dislike		int
+	chanel		[]string
+	target		[]string
+	answers 	[]string
+}
+
+func InitDbpost(db *sql.DB) bool {
+	table := `CREATE TABLE IF NOT EXISTS post
+	(
+	id INT NOT NULL UNIQUE,
+	username VARCHAR(80) NOT NULL,
+	message VARCHAR(80),
+	image VARCHAR(80),
+	date VARCHAR(80),
+	chanel VARCHAR(80),
+	target VARCHAR(80),
+	answers VARCHAR(80),
+	like INT,
+	dislike INT,
+	PRIMARY KEY(id AUTOINCREMENT)
+	);`
+	_, dberr := db.Exec(table)
+	if dberr != nil {
+		log.Fatal(dberr.Error())
+		return false
+	} else {
+		return true
+	}
+}
+
+func convertToString(array []string) string {
+	return strings.Join(array, "|\\/|-_-|\\/|+{}")
+}
+
+func convertToArray(str string) []string {
+	return strings.Split(str, "|\\/|-_-|\\/|+{}")
+}
+
+func AddPost(db *sql.DB, post post) bool {
+	statement, err := db.Prepare("INSERT INTO post(username, message, image, date, chanel, target, answers, like, dislike) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)")
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	fmt.Println(post.username)
+	chanel := convertToString(post.chanel)
+	target := convertToString(post.target)
+	answers := convertToString(post.answers)
+	statement.Exec(post.username, post.message, post.image, post.date, chanel, target, answers, post.like, post.dislike)
+	defer db.Close()
+	return true
+}
+
+func getPost(db *sql.DB, username string, chanels []string) (bool, []post) {
+	output := []post{}
+	UserPost, err := db.Query("SELECT * FROM post WHERE username=?", username)
+	if err != nil {
+		fmt.Println("error in hash")
+	}
+	defer UserPost.Close()
+	for UserPost.Next() {
+		var post post
+		var chanel string
+		var target string
+		var answers string
+		if err := UserPost.Scan(&post.username, &post.message, &post.image, &post.date, chanel, target, answers, &post.like, &post.dislike); err != nil {
+			return false, output
+		}
+		post.chanel = convertToArray(chanel)
+		post.target = convertToArray(target)
+		post.answers = convertToArray(answers)
+		for _,arg := range chanels {
+			if !contains(post.chanel ,arg) {
+				return false, output
+			}
+		}
+		output = append(output, post)
+	}
+	if err = UserPost.Err(); err != nil {
+		return false, output
+	}
+	return true, output
+}
+
+func contains(s []string, str string) bool {
+    for _, v := range s {
+        if v == str {
+            return true
+        }
+    }
+
+    return false
+}
